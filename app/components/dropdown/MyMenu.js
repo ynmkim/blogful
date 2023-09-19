a221181847300b922bcffe1b1a9e409c3ffb2822
김윤미 <younmeet@gmail.com>
2023-09-19 17:27:48 +0900

    my-menu 컨포넌트 분리 전

--- app/components/dropdown/MyMenu.js
+++ app/components/dropdown/MyMenu.js
@@ -1,19 +1,18 @@
-'use client'
-import ButtonLogOut from "../button/ButtonLogOut";
-import { useState } from 'react'
+// 'use client'
+// import ButtonLogOut from "../button/ButtonLogOut";
+// import { useState } from 'react'
 
-export default function MyMenu() {
-  // const state = useState()
-  return (
-    <div className="my-menu">
-      {/* <div className='my-menu-button'></div> */}
-      <div className="my-menu-content">
-        <ul className="my-menu-list">
-          <li className="my-menu-item">
-            <ButtonLogOut />
-        </li>
-      </ul>
-      </div>
-    </div>
-  )
-}
+// export default function MyMenu() {
+//   // const state = useState()
+//   return (
+//     <div className="my-menu">
+//       <div className="my-menu-content">
+//         <ul className="my-menu-list">
+//           <li className="my-menu-item">
+//             <ButtonLogOut />
+//         </li>
+//       </ul>
+//       </div>
+//     </div>
+//   )
+// }
