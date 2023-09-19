e9d4f01ddc7da1c33d7fff2dcdba8e0a4154c09b
김윤미 <younmeet@gmail.com>
2023-09-19 23:31:37 +0900

    iOS <input> focus 시 자동 zoom-in 막기

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,8 @@ import { authOptions } from '@/pages/api/auth/[...nextauth]';
 export const metadata = {
   title: 'blogful',
   description: 'blog app',
+  name: 'viewport',
+  content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
 }
 
 export default async function RootLayout({ children }) {
